0330a510a6bcf28310649eb1367f0b4f63b289d4
stereoIII6 <stereodocbush@gmail.com>
2019-12-09 10:32:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('https://api.jsonbin.io/b/5decb55bbc5ffd040095cb92');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
